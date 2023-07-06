b7370dbd08899574abc7933bd5f839d2d87e8e72
DeerEdge <vislawath.21@gmail.com>
2023-07-06 13:39:01 -0600

    changes

--- 072023/07052023/main.py
+++ 072023/07052023/main.py
@@ -1,6 +1,11 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTabWidget
+import sys
+import os
 
+# path = os.path.abspath("/Users/macbookpro/Documents/GitHub/workspace/072023/07062023/create_widgets.py")
+# sys.path.append(path)
+#
+# import file1
 
 class ui_main_window(object):
     def setup_window(self, main_window):
@@ -10,11 +15,21 @@ class ui_main_window(object):
         self.status_bar = QtWidgets.QStatusBar(main_window)
         self.status_bar.setObjectName("status_bar")
         main_window.setStatusBar(self.status_bar)
-        self.widget_()
 
-    def widget_(self):
-        self.tabWidget = QTabWidget()
-        self.tabWidget.resize(850, 650)
+        self.central_widget = QtWidgets.QWidget(main_window)
+        self.central_widget.resize(850, 650)
+        self.central_widget.setObjectName("display")
+
+        self.tab_widget = QtWidgets.QTabWidget(self.central_widget)
+        self.tab_widget.resize(850, 650)
+
+        self.dash_tab = QtWidgets.QWidget()
+        self.dash_tab.setObjectName("tab1")
+
+        self.widget = QtWidgets.QWidget(self.dash_tab)
+
+        self.tab_widget.addTab(self.dash_tab, " ")
+
 
 if __name__ == "__main__":
     import sys
